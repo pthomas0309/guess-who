17ab0cb7c0983546fb688ffb1e1d88ee101a25e6
pthomas0309 <preston.thomas355@gmail.com>
2021-05-17 18:21:30 -0500

    base mode + images shuffle

--- client.js
+++ client.js
@@ -1,8 +1,15 @@
 console.log('Here are all the available people:', people);
 
-$(document).ready(readyNow)
+$(readyNow);
 
 function readyNow() {
+    displayImages();
+    $('#profileImages').on('click', 'div', evaluateCorrectAnswer);
+}
+
+function displayImages(){
+    console.log('shuffling...', shuffle(people));
+    $('#profileImages').empty();
     for (let person of people){
         console.log(person);
         $('#profileImages').append(`
@@ -11,18 +18,29 @@ function readyNow() {
         </div>
         `)
     }
-    promptUser()
-    $('#profileImages').on('click', 'div', evaluateCorrectAnswer)
+    promptUser();
+}
+
+// Fisher-Yates algorithm
+function shuffle(array) {
+    for (let i = 0; i < array.length; i++) {
+        let randomIndex = Math.floor(Math.random() * (i + 1));
+        let itemAtIndex = array[randomIndex];
+        array[randomIndex] = array[i];
+        array[i] = itemAtIndex;
+    }
+    return array;
+}
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
 }
 
 function promptUser(){
-    let pictureSelector = randomNumber(0, people.length - 1)
-    function randomNumber(min, max){
-        return Math.floor(Math.random() * (1 + max - min) + min);
-    }
+    let pictureSelector = randomNumber(0, people.length - 1);
     for (let person of people){
         if (pictureSelector === people.indexOf(person)){
-            $('#userPrompt').append(`<p>${person.name}</p.`)
+            $('#userPrompt').append(`<p>${person.name}</p.`);
         } else {
             console.log('No matching value');
         }
@@ -30,5 +48,15 @@ function promptUser(){
 }
 
 function evaluateCorrectAnswer(){
-    console.log(`picking...${$(this).data('name')}`)
+    let nameOfSelection = $(this).data('name');
+    let correctAnswer = $('#userPrompt').children().text()
+    if (nameOfSelection === correctAnswer){
+        alert("You got it! Go again?")
+        displayImages();
+        $('#userPrompt').empty();
+        promptUser();
+    }
+    else {
+        alert("Not quite right, give it another shot.");
+    }
 }

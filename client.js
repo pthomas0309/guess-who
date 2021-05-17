6ec73afab821013e2f3b21512553360e7697bb13
pthomas0309 <preston.thomas355@gmail.com>
2021-05-17 16:55:21 -0500

    display the randomly generated name of one of the people in the pictures

--- client.js
+++ client.js
@@ -5,10 +5,30 @@ $(document).ready(readyNow)
 function readyNow() {
     for (let person of people){
         console.log(person);
-        $('body').append(`
-        <div>
+        $('#profileImages').append(`
+        <div data-name=${person.name}>
             <img src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of ${person.name}">
         </div>
         `)
     }
+    promptUser()
+    $('#profileImages').on('click', 'div', evaluateCorrectAnswer)
+}
+
+function promptUser(){
+    let pictureSelector = randomNumber(0, people.length - 1)
+    function randomNumber(min, max){
+        return Math.floor(Math.random() * (1 + max - min) + min);
+    }
+    for (let person of people){
+        if (pictureSelector === people.indexOf(person)){
+            $('#userPrompt').append(`<p>${person.name}</p.`)
+        } else {
+            console.log('No matching value');
+        }
+    }
+}
+
+function evaluateCorrectAnswer(){
+    console.log(`picking...${$(this).data('name')}`)
 }
